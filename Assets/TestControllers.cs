40e884566dec80be2a5afa75db4df56a82803da6
jissmon <jisshub>
2020-08-19 18:15:45 +0530

    changes in asset
    new script file created

---
+++ Assets/TestControllers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestControllers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int one;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
